b7f40aea93364ad434e24af1859f06ef69be1f86
sahra <sahra.ogul@study.hs-duesseldorf.de>
2026-02-04 23:30:39 +0100

    Implement fair_sharer function and add corresponding tests

--- fair_sharer/tests/testfairsharer.py
+++ fair_sharer/tests/testfairsharer.py
@@ -1,19 +1,20 @@
-from fairsharer import my_module
+from fairsharer.myfairsharer import fair_sharer
+
 
 
 def test_fair_sharer_one_iteration():
     values = [0, 1000, 800, 0]
-    result = my_module.fair_sharer(values, 1)
+    result = fair_sharer(values, 1)
     assert result == [100, 800, 900, 0]
 
 
 def test_fair_sharer_two_iterations():
     values = [0, 1000, 800, 0]
-    result = my_module.fair_sharer(values, 2)
+    result = fair_sharer(values, 2)
     assert result == [100, 890, 720, 90]
 
 
 def test_no_iterations():
     values = [1, 2, 3]
-    result = my_module.fair_sharer(values, 0)
+    result = fair_sharer(values, 0)
     assert result == values
